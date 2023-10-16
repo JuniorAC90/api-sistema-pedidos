f1cad5ccd83119d8f5f834e772a10431c39c1447
aloizio <aloizio_junior@hotmail.com>
2023-10-16 20:40:12 -0300

    Iniciando projeto

---
+++ src/main/java/com/acjunior/apisistemapedidos/ApiSistemaPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.acjunior.apisistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acjunior/apisistemapedidos/ApiSistemaPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acjunior.apisistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
